ee5bd71ce5d3bdcea311ce1afa00930cb532a75b
Todd Engen <engentm@outlook.com>
2024-12-07 15:50:04 -0600

    Commit

--- day7.ps1
+++ day7.ps1
@@ -19,16 +19,13 @@ $data = get-content .\input_day7.txt
 
 $table = $data -split "\r\n"
 
-
 # Part 1
-# Number of different equations is 2^(operands-1)
+# Find number of combinations of + and * based on spaces between digits
+# The test values are saved in a hash table and the test results are updated to $true.
 
-
-$total = 0 
 $hashmap = @{}
 
 for ($r=0; $r -lt $table.Count; $r++){
-
 
  $test = $table[$r] -split ":"
 
@@ -43,7 +40,7 @@ for ($r=0; $r -lt $table.Count; $r++){
  $operators = new-object system.collections.stack
  $numberstack = new-object system.collections.stack
 
-# Iterate through all possible combinations of operators
+# Go through all possible combinations of operators
 for ($i = 0; $i -lt [Math]::Pow($chars.Length, $spacecount); $i++) {
 
  $operators.Clear()
@@ -54,17 +51,14 @@ for ($i = 0; $i -lt [Math]::Pow($chars.Length, $spacecount); $i++) {
   for ($j = 0; $j -lt $spacecount; $j++) {
     $index = [Math]::Floor(($i / [Math]::Pow($chars.length, $j)) % $chars.Length)
     #write-host $index
-    $combination += $chars[$index]
     $operators.Push($chars[$index])
-
   }
-  # Evaluate each formula
 
-
+  # Reset the list of numbers in the stack
   for ($k=0; $k -lt $numbers.Length ; $k++) { $operands.Push($numbers[$k])}
 
- 
- while ($operators.count -gt 0) {   
+  # Take two number, apply the operator, and push back onto the stack. Repeat.
+  while ($operators.count -gt 0) {   
     $symbol = $operators.pop()
    
     $n1 = [int64]$numberstack.pop()
@@ -88,4 +82,63 @@ $trueKeys = $hashmap.Keys | Where-Object { $hashmap[$_] -eq $true }
 $truekeys | measure-object -sum
 
 
+# Part 2: 
+
+$hashmap = @{}
+
+for ($r=0; $r -lt $table.Count; $r++){
+
+ $test = $table[$r] -split ":"
+
+ $hashmap.add($test[0],$false)
+
+ $numbers = $test[1].trim() -split " " 
+
+ $spacecount = ($test[1].trim() -split '\s+' | Measure-Object -Line).Lines - 1
+ 
+ $chars = "+*"
+
+ $operators = new-object system.collections.stack
+ $numberstack = new-object system.collections.stack
+
+# Go through all possible combinations of operators
+for ($i = 0; $i -lt [Math]::Pow($chars.Length, $spacecount); $i++) {
+
+ $operators.Clear()
+ $numberstack.Clear() 
+ 
+ for ($n = $numbers.Length - 1; $n -ge 0; $n--) {  $numberstack.Push($numbers[$n]) }
+
+  for ($j = 0; $j -lt $spacecount; $j++) {
+    $index = [Math]::Floor(($i / [Math]::Pow($chars.length, $j)) % $chars.Length)
+    #write-host $index
+    $operators.Push($chars[$index])
+  }
+
+  # Reset the list of numbers in the stack
+  for ($k=0; $k -lt $numbers.Length ; $k++) { $operands.Push($numbers[$k])}
+
+  # Take two number, apply the operator, and push back onto the stack. Repeat.
+  while ($operators.count -gt 0) {   
+    $symbol = $operators.pop()
+   
+    $n1 = [int64]$numberstack.pop()
+    $n2 = [int64]$numberstack.pop()
+
+     switch ($symbol) {
+    '+' { $numberStack.Push($n1 + $n2) }
+    '*' { $numberStack.Push($n1 * $n2) }
+    }
+
+ 
+  }
+    $result = $numberstack.pop()
+    if ($result -eq $test[0]) {Write-host "Match $test $operators"; $hashmap.($test[0]) = $true}  else {}
+}
+}
+
+$hashmap
+$trueKeys = $hashmap.Keys | Where-Object { $hashmap[$_] -eq $true }
+
+$truekeys | measure-object -sum
 
